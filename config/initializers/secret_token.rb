8b56d4df1f05f0a34b237c7595bed1c11ef902a6
Rey Techy <zet.david2@gmail.com>
2014-08-14 11:25:43 +0800

    Testing
    Webtech Test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testing::Application.config.secret_key_base = '95dfed567e3b488903368643cc314ff352fa7ea26598258a3133cf679d481259c8c03a960f938d8fdee14aa710ce2b98833585ee762deb6ed1b05e49f913f275'
